8105d6468f9ca951004deac927257937ded0c59a
atakanoguz <atakan17171717@hotmail.com>
2022-08-04 17:03:06 +0300

    model class created

---
+++ src/main/java/com/atakanoguzdev/springbootmicroservice3apigateway/model/Role.java
@@ -0,0 +1,7 @@
+package com.atakanoguzdev.springbootmicroservice3apigateway.model;
+
+public enum Role
+{
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/atakanoguzdev/springbootmicroservice3apigateway/model/User.java
@@ -0,0 +1,39 @@
+package com.atakanoguzdev.springbootmicroservice3apigateway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "create_time", nullable = false)
+    private LocalDateTime createTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+}
