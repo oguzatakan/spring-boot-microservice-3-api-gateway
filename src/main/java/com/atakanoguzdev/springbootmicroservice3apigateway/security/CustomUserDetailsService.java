1e8ee2a082cd4cf949f4f6517dbfb8dd750d9312
atakanoguz <atakan17171717@hotmail.com>
2022-08-10 17:14:54 +0300

    custom authorities details service created

---
+++ src/main/java/com/atakanoguzdev/springbootmicroservice3apigateway/security/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.atakanoguzdev.springbootmicroservice3apigateway.security;
+
+import com.atakanoguzdev.springbootmicroservice3apigateway.model.User;
+import com.atakanoguzdev.springbootmicroservice3apigateway.service.UserService;
+import com.atakanoguzdev.springbootmicroservice3apigateway.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService
+{
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        //authority
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRole().name()));
+
+        return UserPrincipal.builder()
+                .user(user)
+                .id(user.getId())
+                .username(username)
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+
+    }
+}
